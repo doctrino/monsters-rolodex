c2cae000d1c479074e72dc842dbb9f0ad7b8488d
Anders Albert <anders.albert@arundo.com>
2021-03-13 16:35:56 +0100

    feat: Added searchfield for app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
 import {CardList} from "./components/card-list/card-list.component";
+import {SearchBox} from "./components/search-box/search-box.component";
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ""
         }
     }
 
@@ -16,12 +18,19 @@ class App extends Component {
             .then(users => this.setState({monsters: users}))
     }
 
+    updateSearchField = (e) => {
+        this.setState({searchField: e.target.value})
+    }
+
     render() {
+        const {monsters, searchField} = this.state;
+        const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
         return (
             <div className="App">
-                <CardList monsters={this.state.monsters}>
-                </CardList>
-
+                <h1>Monsters Rolodex</h1>
+                <SearchBox placeholder="search monsters"
+                           handleChange={this.updateSearchField}/>
+                <CardList monsters={filteredMonsters}/>
             </div>
         );
     }
